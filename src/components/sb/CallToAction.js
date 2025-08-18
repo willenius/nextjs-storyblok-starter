8c275aa44748214d6d12029fb44acdd2b3097700
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-08-18 14:01:09 +0200

    fix call to action

--- src/components/sb/CallToAction.js
+++ src/components/sb/CallToAction.js
@@ -1,17 +1,16 @@
 
 //color blue, black, red
 
-const colors = {
-    blue: "bg-blue-500 text-white hover:bg-blue-600",
-    black: "bg-black text-white hover:bg-gray-800",
-    red: "bg-red-500 text-white hover:bg-red-600",
-}
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 
 export default function CallToAction({ blok }) {
-    const colorClass = colors[blok.color]
     return (
-        <button className={`${colorClass} px-4 py-2 rounded-md`}>
-            {blok.label}
-        </button>
+        <Link href={blok.link.url}>
+            <Button variant={blok.color}>
+                {blok.title}
+            </Button>
+        </Link>
     );
 }
--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -17,6 +17,9 @@ const buttonVariants = cva(
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+        blue: "bg-blue-500 text-white hover:bg-blue-600",
+        black: "bg-black text-white hover:bg-gray-800",
+        red: "bg-red-500 text-white hover:bg-red-600",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
