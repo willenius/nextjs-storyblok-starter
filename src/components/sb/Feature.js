8325fb5e0a67e683cd008532a1a9d7a838e584a1
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-08-18 13:53:24 +0200

    implement basic components from shad cn

--- src/components/sb/Feature.js
+++ src/components/sb/Feature.js
@@ -1,25 +1,26 @@
 import { storyblokEditable } from "@storyblok/react/rsc";
 import ServerComponent from "./ServerComponent";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Feature({ blok }) {
   return (
-    <div
-      className="feature shadow-md p-4 rounded-md w-75"
+    <Card
+      className="w-75 pt-0"
       {...storyblokEditable(blok)}
     >
       <img
         src={`https://picsum.photos/250/250?random=${blok._uid}`}
         alt={blok.name}
-        className="w-full aspect-square object-cover rounded-md"
+        className="w-full aspect-square object-cover rounded-t-md"
       />
+      <CardContent className="flex flex-col gap-2">
       <h2 className="text-2xl font-bold">{blok.name}</h2>
       <p className="text-gray-600">{blok.description}</p>
-      <div className="flex flex-col gap-2">
         {blok?.cta?.map((nestedBlok) => (
           <ServerComponent blok={nestedBlok} key={nestedBlok._uid} />
         ))}
-      </div>
+      </CardContent>
 
-    </div>
+    </Card>
   );
 }
--- src/components/sb/Teaser.js
+++ src/components/sb/Teaser.js
@@ -1,8 +1,17 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+
 export default function Teaser({ blok }) {
-    return (
-        <div className="teaser shadow-md p-4 rounded-md bg-white border border-gray-200">
-            <h2 className="text-2xl font-bold">{blok.headline}</h2>
-            <p className="text-gray-600">{blok.description}</p>
-        </div>
-    );
-    }
+  return (
+    <Card className="teaser">
+      <CardHeader>
+        <CardTitle className="text-2xl">{blok.headline}</CardTitle>
+        <CardDescription>{blok.description}</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+}
